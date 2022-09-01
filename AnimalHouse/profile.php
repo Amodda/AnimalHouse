4f4407c9922955dc5bd9a034cb35cd3caf5fb0af
GioeleMandrioli <gioele.mandrioli@studio.unibo.it>
2022-09-01 11:56:34 +0200

    preferenze in profilo funzionanti con array

--- AnimalHouse/profile.php
+++ AnimalHouse/profile.php
@@ -6,8 +6,24 @@ if(!isset($_SESSION['user'])){
 }
 
 $jsonData = file_get_contents("usersTest.json");
-$_SESSION["users"] = json_decode($jsonData, true);
+$users = json_decode($jsonData, true);
+for($i = 0; $i <= count($users)-1; $i++){
+    if($users[$i]["username"] == $_SESSION["user"]["username"]){
+        $gamePoint = [ "quiz" => $users[$i]["gamesPoints"]["quiz"],
+                        "hangman" => $users[$i]["gamesPoints"]["hangman"],
+                        "memory"  => $users[$i]["gamesPoints"]["memory"]];
+    }
+}
 
+$readfile = file_get_contents("favourites.json");
+$favList =json_decode($readfile, true);
+for($i = 0; $i <= count($favList)-1; $i++){
+    if($_SESSION["user"]["username"] == $favList[$i]["username"]){
+        $_SESSION["favList"] = $favList[$i]["preferences"];
+    }
+}
+
+// creare sezione di aggiunta animali preferiti
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -18,8 +34,10 @@ $_SESSION["users"] = json_decode($jsonData, true);
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="css/animalHouse.css">
     <link rel="stylesheet" href="css/profile.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
+    <script src="http://code.jquery.com/ui/1.11.0/jquery-ui.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-    <title>Document</title>
+    <script type="text/javascript" src="js/profileManager.js"></script>
 </head>
 <body>
 <nav class="navbar navbar-expand-lg navbar-light bg-light sticky-top shadow">
@@ -32,7 +50,7 @@ $_SESSION["users"] = json_decode($jsonData, true);
             <ul class="navbar-nav ms-auto">
 
               <li class="nav-item">
-                <a class="nav-link active" href="#">Home</a>
+                <a class="nav-link active" href="index.php">Home</a>
               </li>
               <?php
                     if(!isset($_SESSION['user'])){
@@ -49,7 +67,7 @@ $_SESSION["users"] = json_decode($jsonData, true);
                             echo '<a class="nav-link" href="forum.php">Forum</a>';
                         echo '</li>';
                         echo '<li class="nav-item">';
-                            echo '<a class="nav-link" href="profile.php">Profile</a>';
+                            echo '<a class="nav-link" href="#">Profile</a>';
                         echo '</li>';
                         echo '<li class="nav-item dropdown">';
                         echo '<a class="nav-link dropdown-toggle" href="#" id="navbarDarkDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">Welcome '.$_SESSION['user']['name'].'</a>';
@@ -69,30 +87,87 @@ $_SESSION["users"] = json_decode($jsonData, true);
     <div class="container">
         <div class="row" id="name">
             <div class="col-lg-12 col-sm-6"></div>
-            <h1> Gioele Mandrioli </h1>
-            <h3> @nomeutente</h3>
+            <h1><?php echo $_SESSION['user']['name']." ". $_SESSION['user']['lastname']?></h1>
+            <h3 id="user"><?php echo $_SESSION['user']['username']?></h3>
         </div>
         <div class="row gap-1">
             <div class="col-md-3 col-sm-2 box">
-                <div>nome
-                    Cognome
-                    Email
-                    NomeUtente
+                <h3>Information: </h3>
+                <div class="row">
+                    <div class="col"> Name: <b><?php echo $_SESSION['user']['name']; ?> </b></div>
+                </div>
+                <div class="row">
+                    <div class="col"> Lastname: <b><?php echo $_SESSION['user']['lastname']; ?> </b></div>
+                </div>
+                <div class="row">
+                    <div class="col"> Username: <b><?php echo $_SESSION['user']['username']; ?> </b></div>
+                </div>
+                <div class="row">
+                    <div class="col"> E-mail: <b><?php echo $_SESSION['user']['email']; ?> </b></div>
                 </div>
             </div>
-            <div class="col-md-4 col-sm-2 box">
-                <div>Preferences</div>
+            <div class="col-md-5 col-sm-2 box">
+                <h3>Animals list: </h3>
+                <div class="table-wrapper-scroll-y my-custom-scrollbar">
+                    <table class="table table-bordered table-striped mb-0" id="favTable">
+                        <thead>
+
+                        </thead>
+                        <tbody id="tableBody" >
+                        <?php 
+                            if(isset($_SESSION['favList'])){
+                                $temp = $_SESSION['favList'];
+                                for($i=0; $i< count($_SESSION['favList']); $i++){
+                                echo "<tr>";
+                                echo '<td class="rowanimal">'.$temp[$i].'</td>';
+                                echo '<td><a href="#" class="remove" onClick="deletePref()"> [-] </td>';
+
+                                echo "</tr>";
+                                }
+                            }
+
+                            
+                        ?>
+                        </tbody>
+                        <tfoot>
+                            <tr>
+                                <td><a href="#" id="add">Add animals: </a></td>
+                            </tr>
+                        </tfoot>
+                    </table>
+                </div>
             </div>
             <div class="col-md-3 col-sm-2 box">
-                <div>Games</div>
+                <h3>Games</h3>
+                <div class="row text-align-center">
+                    <div class="col">Quiz score: <b><?php echo $gamePoint["quiz"]?></b></div>
+                </div> 
+                <div class="row">
+                    <div class="col">Hangman score: <b><?php echo $gamePoint["hangman"]?></b></div>
+                </div> 
+                <div class="row">
+                    <div class="col">Memory score: <b><?php echo $gamePoint["memory"]?></b></div>
+                </div>
+                <div class="row">
+                    <div class="col">Total score: <b>
+                        <?php 
+                            echo $gamePoint["quiz"]+$gamePoint["hangman"]+$gamePoint["memory"]?></b></div>
+                </div>
             </div>
         </div>
         
         
         
     </div>
+    <hr>
+    <div id="sezP" style="display: none;">
+        <h2> Scegli i tuoi animali preferiti: </h2>
+        <section class="grid" id="animals"></section>
+        <button id="salva_pref" onclick="save()"> Salva Preferiti</button>         
+    </div>
+    
     
 
-
 </body>
+
 </html>
