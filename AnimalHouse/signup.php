91c6929c24cdfe9407aa7be0411c95212dd7b66a
Alessandro Modelli <alessandro.modelli99@gmail.com>
2022-09-07 12:50:47 +0200

    fixes

--- AnimalHouse/signup.php
+++ AnimalHouse/signup.php
@@ -23,7 +23,7 @@ session_start();
           <div class="collapse navbar-collapse" id="navbarResponsive">
             <ul class="navbar-nav ms-auto">
               <li class="nav-item active">
-                <a class="nav-link" href="#">Home</a>
+                <a class="nav-link" href="index.php">Home</a>
               </li>
 
     
